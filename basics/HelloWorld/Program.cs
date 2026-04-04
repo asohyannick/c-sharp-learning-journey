33b4916b3d55d114f5681b466cfb1f2c6649e088
asohyannick <asoh@cskytelecoms.com>
2026-04-04 22:10:27 +0100

    I've completed the file, file info API within my learning C# journey successfully

--- basics/HelloWorld/Program.cs
+++ basics/HelloWorld/Program.cs
@@ -1,19 +1,21 @@
-﻿var path = @"c:\somefile.jpg";
-File.Copy("c:\\myfile.jpg", "d:\\temp\\myfile.jpg", true);
-File.Delete(path);
-if(File.Exists(path))
-{
-    //
-}
+﻿Directory.CreateDirectory(@"c:\\projects\Csharp");
+var files = Directory.GetFiles(@"c:\\projects\Csharp", ", ", SearchOption.AllDirectories);
+foreach(var file in files) 
+System.Console.WriteLine(file);
 
-var content = File.ReadAllText(path);
-var fileInfo = new FileInfo(path);
-fileInfo.CopyTo("...");
-fileInfo.Delete();
+var directories = Directory.GetDirectories(@"c:\\projects\C#", ",*", SearchOption.AllDirectories);
+foreach(var directory in directories)
+System.Console.WriteLine(directory);
 
-if(fileInfo.Exists)
-{
-    // 
-}
+Directory.Exists("...");
+var directoryInfo = DirectoryInfo("...");
+directoryInfo.GetFiles();
+directoryInfo.GetDirectory();
 
-fileInfo.Read
+var path = @"c:\Projects\C#\helloworld";
+var dotIndex = path.IndexOf(',');
+var extension = path.Substring(dotIndex);
+System.Console.WriteLine("Extension:" + Path.GetExtension(path));
+System.Console.WriteLine("File Name: " + Path.GetFileName(path));
+System.Console.WriteLine("File Name with Extension:" + Path.GetFileNameWithoutExtension(path));
+System.Console.WriteLine("Directory Name:" + Path.GetDirectoryName(path));

47995056984a7b4b5fc5196240424b4f35f7aaf4
asohyannick <asoh@cskytelecoms.com>
2026-04-04 16:47:13 +0100

    I've covered the random class in C# in great detail successfully

--- basics/HelloWorld/Program.cs
+++ basics/HelloWorld/Program.cs
@@ -1,79 +1,8 @@
-﻿
-int hour = 10;
-bool isGoldCustomer = true;
-float price = isGoldCustomer ? 19.95f : 29.95f;
-if (hour > 0 && hour < 12)
-{
-  Console.WriteLine("It's morning");
-}
-else if (hour >= 12 && hour < 18)
-{
-  Console.WriteLine("It's afternoon");
-}
-else
-{
-  Console.WriteLine("It's evening.");
-}
+﻿var random = new Random();
+const int passwordLength = 10;
+char[] buffer = new char[passwordLength];
+for(var i = 0; i < passwordLength; i ++)
+buffer[i] = (char)('a' + random.Next(0, 20));
 
-Console.WriteLine(price);
-
-var season = Season.Autumn;
-switch (season)
-{
-  case Season.Autumn:
-    Console.WriteLine("It's autumn and a beautiful season");
-    break;
-  case Season.Summer:
-    Console.WriteLine("It's perfect to go to beach");
-    break;
-  default:
-    Console.WriteLine("Unknown Season");
-    break;
-}
-
-
-for (var i = 1; i <= 10; i++)
-{
-  if (i % 2 == 0)
-    Console.WriteLine(i);
-}
-
-for (var i = 10; i >= 1; i--)
-{
-  Console.WriteLine(i);
-}
-
-var name = "John Smith";
-for (var i = 0; i < name.Length; i++)
-{
-  System.Console.WriteLine(name[i]);
-}
-
-foreach (var character in name)
-{
-  Console.WriteLine(character);
-}
-
-var numbers = new int[] { 1, 2, 3, 4 };
-foreach (var number in numbers)
-{
-  Console.WriteLine(number);
-}
-
-var y = 0;
-while (y <= 10)
-{
-  if (y % 2 == 0)
-    break;
-  Console.WriteLine(y);
-}
-
-while (true)
-{
-  Console.WriteLine("Type your name");
-  var input = Console.ReadLine();
-  if (!String.IsNullOrWhiteSpace(input))
-    continue;
-  Console.WriteLine("@Echo:", input);
-  break;
-}
+var password = new string(buffer);
+Console.WriteLine(password);
--- basics/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ basics/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fddd366a8e34c2504dce2212c16e2c94fbe052f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd19043beb5490ec59e88b5c1995955adce6b99")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
